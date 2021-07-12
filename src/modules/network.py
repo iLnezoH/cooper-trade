08231cac2200a026ff420622f6e0f8dcd41fc930
Hozen <Hozen@live.com>
2021-07-12 21:23:05 +0800

    adjacency information Entropy

--- src/modules/network.py
+++ src/modules/network.py
@@ -1,8 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import collections
+import math
 
-from src.modules.utils import getEntropy
+from networkx.classes.function import neighbors
+from networkx.readwrite.json_graph import adjacency
+
+from src.modules.utils import getEntropy, getAdjacencyDegree, getStrength, getSelectionProbability, getAdjacencyEntropy
 
 
 class Net():
@@ -10,6 +14,9 @@ class Net():
         self.data = data
         self.initialEdges = []
         self.G = self._generateNet(data)
+        self._strengths = None
+        self._adjacencyDegrees = None
+        self._Es = None
 
     def _generateNet(self, data):
 
@@ -17,9 +24,9 @@ class Net():
         tradeLogs = data.getMergedData()
 
         for tradeObj, tradeValue in tradeLogs.items():
-            G.add_node(tradeObj[0], label = data.getCountryName(tradeObj[0]))
-            G.add_node(tradeObj[1], label = data.getCountryName(tradeObj[1]))
-            G.add_edge(tradeObj[0], tradeObj[1], weight = tradeValue)
+            G.add_node(tradeObj[0], label=data.getCountryName(tradeObj[0]))
+            G.add_node(tradeObj[1], label=data.getCountryName(tradeObj[1]))
+            G.add_edge(tradeObj[0], tradeObj[1], weight=tradeValue)
 
         '''
         allParticipants = data.getAllParticipants()
@@ -54,6 +61,11 @@ class Net():
 
         return G
 
+    def freshGraph(self):
+        self._strengths = None
+        self._adjacencyDegrees = None
+        self._Es = None
+
     def _repeatEdgeCheck(self, u, v):
         for (_u, _v) in self.initialEdges:
             if _u == u and _v == v:
@@ -70,6 +82,101 @@ class Net():
         E0 = getEntropy([x/amount for x in distribute])
 
         return E0
+
+    def getStrength(self, node, l=0.75):
+        return getStrength(self.G, node, l)
+
+    def getAdjacencyDegree(self, node, theta=0.75, l=0.75):
+        return getAdjacencyDegree(self.G, node, theta, l)
+
+    def getSelectionProbability(self, i, j, theta=0.75, l=0.75):
+        return getSelectionProbability(self.G, i, j, theta, l)
+
+    def getAdjacencyEntropy(self, i, theta=0.75, l=0.75):
+        return getAdjacencyEntropy(self.G, i, theta, l)
+
+    def getStrengths(self, l=0.75):
+        if (self._strengths is not None):
+            return self._strengths
+        strengths = {}
+        for node in self.G.nodes:
+            strengths[node] = self.getStrength(node, l)
+
+        self._strengths = strengths
+
+        return self._strengths
+
+    def getAdjacencyDegrees(self, theta=0.75, l=0.75):
+        if self._adjacencyDegrees is not None:
+            return self._adjacencyDegrees
+
+        _adjacencyDegrees = {}
+        strengths = self.getStrengths(l)
+
+        for node in self.G.nodes:
+            successors = self.G.successors(node)
+            predecessors = self.G.predecessors(node)
+
+            in_strengths = sum([strengths[in_node]
+                               for in_node in predecessors])
+            out_strengths = sum([strengths[out_node]
+                                for out_node in successors])
+
+            _adjacencyDegrees[node] = theta * \
+                in_strengths + (1 - theta) * out_strengths
+
+        self._adjacencyDegrees = _adjacencyDegrees
+
+        return _adjacencyDegrees
+
+    def getAdjacencyEntropies(self, theta=0.75, l=0.75):
+        if (self._Es is not None):
+            return self._Es
+        Es = {}
+
+        strengths = self.getStrengths()
+        adjacencyDegrees = self.getAdjacencyDegrees()
+
+        for i in self.G.nodes:
+            neighbors = self.getNeighbors(i)
+            E = 0
+            strength = strengths[i]
+            for neighbor in neighbors:
+                p = strength / adjacencyDegrees[neighbor]
+                E += abs(p * math.log(p, 2))
+
+            Es[i] = E
+
+        return Es
+
+    def getSortedEntropies(self):
+        entropiesDict = self.getAdjacencyEntropies()
+        entropiesArr = [
+            {
+                "name": self.data.getCountryName(code),
+                "code": code,
+                "E": E
+            } for code, E in entropiesDict.items()
+        ]
+
+        return sorted(entropiesArr, key=lambda e: e["E"], reverse=True)
+
+    def getNeighbors(self, node):
+        successors = self.G.successors(node)
+        predecessors = self.G.predecessors(node)
+
+        return set((*successors, *predecessors))
+
+    def drawEntropiesBar(self, count=20, width=0.8, color="b"):
+        sortedEntropies = self.getSortedEntropies()
+        countries = []
+        Es = []
+        for item in sortedEntropies:
+            countries.append(item["name"])
+            Es.append(item["E"])
+
+        plt.figure(figsize=(20, 5))
+        plt.bar(countries[:20], Es[:20], width=width, color=color)
 
     def getDegreeCount(self):
         degree_sequence = sorted(
@@ -104,7 +211,7 @@ class Net():
             for item in nodeStrength
         ]
 
-        plt.figure(3, figsize=(30, 30))
+        plt.figure(figsize=(30, 30))
         pos = nx.random_layout(G)
         # pos = nx.spiral_layout(G)
         nx.draw(G, pos, with_labels=False, node_size=node_sizes)
--- src/modules/utils.py
+++ src/modules/utils.py
@@ -7,3 +7,42 @@ def getEntropy(ps):
             raise ValueError("p must in (0,1)")
     pLogP = [-p * math.log(p, 2) for p in ps if p > 0]
     return sum(pLogP)
+
+
+def getStrength(G, node, l=0.75):
+    in_edges = G.in_edges(node, data="weight")
+    out_edges = G.out_edges(node, data="weight")
+
+    in_strength = sum([data[2] for data in in_edges])
+    out_strength = sum([data[2] for data in out_edges])
+
+    return l * in_strength + (1 - l) * out_strength
+
+
+def getAdjacencyDegree(G, node, theta=0.75, l=0.75):
+    successors = G.successors(node)
+    predecessors = G.predecessors(node)
+
+    in_degree = sum([getStrength(G, node, l) for node in predecessors])
+    out_degree = sum([getStrength(G, node, l) for node in successors])
+
+    return theta * in_degree + (1 - theta) * out_degree
+
+
+def getSelectionProbability(G, i, j, theta=0.75, l=0.75):
+    return getStrength(G, i, l) / getAdjacencyDegree(G, j, theta, l)
+
+
+def getAdjacencyEntropy(G, i, theta=0.75, l=0.75):
+    successors = G.successors(i)
+    predecessors = G.predecessors(i)
+
+    neighbors = set((*successors, *predecessors))
+
+    E = 0
+
+    for neighbor in neighbors:
+        p = getSelectionProbability(G, i, neighbor, theta, l)
+        E += abs(p * math.log(p, 2))
+
+    return E
