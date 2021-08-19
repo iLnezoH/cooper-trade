ab18bb037e7e3df17f64c4f22ef0189ef7164308
iLnezoH <Accenzer@gamil.com>
2021-08-19 11:40:22 +0800

    data(UN): get 2016-2020 data from API

--- src/modules/UN-API.py
+++ src/modules/UN-API.py
@@ -12,33 +12,34 @@ base_url = 'http://comtrade.un.org/api/get'
 
 def save_data(year, data):
     t = time.time()
-    with open('../data/' + str(int(t)) + str(year) + '-world-copper-2063-trade.json', 'w') as f:
+    with open('../data/' + str(int(t)) + '-' + str(year) + '-world-copper-2063-trade.json', 'w') as f:
         json.dump(data, f)
 
 def get_cooper_trade_data(year):
+    _year = ','.join(year)
     copper_trade_data = []
-    to_iter_cods = UN_code[81:]
+    to_iter_cods = UN_code[181:]
     failed_req = []
     try: 
-        while True:
-            for item in to_iter_cods:
-                res = get_cooper_trade_data_by_reporter(code=item['id'], year=year)
-                if res is None:
-                    print('failed: ', item['id'])
-                    failed_req.append(item)
-                else:
-                    print('success: ', item['id'])
-                    copper_trade_data.append(res)
+        # while True:
+        for item in to_iter_cods:
+            res = get_cooper_trade_data_by_reporter(code=item['id'], year=_year)
+            if res is None:
+                print('failed: ', item['id'])
+                failed_req.append(item)
+            else:
+                print('success: ', item['id'])
+                copper_trade_data.append(res)
 
-            to_iter_cods = failed_req
-            failed_req = []
+        # to_iter_cods = failed_req
+        # failed_req = []
 
-            if len(failed_req) == 0: break
+            # if len(failed_req) == 0: break
     except Exception as e:
         print(traceback.print_exc())
-        save_data(year, copper_trade_data)
+        save_data(_year, copper_trade_data)
     else:
-        save_data(year, copper_trade_data)
+        save_data(_year, copper_trade_data)
 
 def get_cooper_trade_data_by_reporter(code, year):
     params = {"type": "C", 'freq': 'A', 'px': 'HS', 'cc': 2603, 'rg': 'all', 'p': 'all', 'ps': year, 'r': code}
@@ -58,4 +59,4 @@ def valide_response(res):
 
     return False
 
-get_cooper_trade_data(2016)
+get_cooper_trade_data(['2017','2018','2020'])
