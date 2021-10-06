9703a896a01f8cba70dd005da7d3eba9ee8baa5d
Hozen <Hozen@live.com>
2021-10-06 20:37:37 +0800

    fix(Data): fix the issue that causing TradeValue would be get mean incorrectly

--- src/modules/data.py
+++ src/modules/data.py
@@ -6,23 +6,22 @@ from pandas.core.frame import DataFrame
 class Data():
     def __init__(self, path) -> None:
         self.data = self._preProcessing(path)
-        self._allReporters = None
-        self._allPartners = None
         self._allParticipants = None
         self._mergedData = None
 
     def _preProcessing(self, path) -> DataFrame:
         extentionName = pathlib.Path(path).suffix.lower()
+
         if extentionName == '.csv':
-            resourceData = pd.read_csv(path)
+            logs = pd.read_csv(path)
         elif extentionName == '.json':
-            resourceData = pd.read_json(path)
-        data = (
-            resourceData[resourceData["Partner Code"] != 0]
-            .loc[:, ["Reporter Code", "Reporter", "Partner Code", "Partner", "Trade Flow", "Trade Value (US$)"]]
-        )
-        data = self._drop_self_lop_data(data)
+            logs = pd.read_json(path)
+
+        data = self._drop_self_lop_data(logs)
         data = self._drop_union_data(data)
+
+        data.index = range(1, len(data)+1)
+
         return data
 
     def _drop_self_lop_data(self, data):
@@ -30,55 +29,59 @@ class Data():
 
     def _drop_union_data(self, data):
         union_codes = [
-            "490", "899", "975", "97", "568",
-            490, 899, 975, 97, 568
+            "0", "490", "899", "975", "97", "568",
+            0, 490, 899, 975, 97, 568
         ]
+
         return data[~(data["Reporter Code"].isin(union_codes) | data["Partner Code"].isin(union_codes))]
 
-    def getAllReporters(self) -> DataFrame:
-        if (self._allReporters is not None):
-            return self._allReporters
+    @property
+    def allReporters(self):
 
-        self._allReporters = (
+        allReporters = (
             self.data.loc[:, ["Reporter Code", "Reporter"]]
             .drop_duplicates()
         )
 
-        self._allReporters.rename(
-            columns={"Reporter Code": "Code", "Reporter": "name"}, inplace=True)
+        allReporters.rename(
+            columns={"Reporter Code": "Code", "Reporter": "Name"}, inplace=True)
 
-        return self._allReporters
+        allReporters.index = range(1, len(allReporters) + 1)
 
-    def getAllPartners(self) -> DataFrame:
-        if (self._allPartners is not None):
-            return self._allPartners
+        return allReporters
 
-        self._allPartners = (
+    @property
+    def allPartners(self) -> DataFrame:
+
+        allPartners = (
             self.data.loc[:, ["Partner Code", "Partner"]]
             .drop_duplicates()
         )
 
-        self._allPartners.rename(
-            columns={"Partner Code": "Code", "Partner": "name"}, inplace=True)
+        allPartners.rename(
+            columns={"Partner Code": "Code", "Partner": "Name"}, inplace=True)
 
-        return self._allPartners
+        allPartners.index = range(1, len(allPartners) + 1)
 
-    def getAllParticipants(self) -> DataFrame:
-        if (self._allParticipants is not None):
-            return self._allParticipants
+        return allPartners
 
-        allReporters = self.getAllReporters()
-        allPartners = self.getAllPartners()
+    @property
+    def allParticipants(self) -> DataFrame:
 
-        self._allParticipants = (
+        allReporters = self.allReporters
+        allPartners = self.allPartners
+
+        allParticipants = (
             pd.concat([allReporters, allPartners])
             .drop_duplicates()
         )
 
-        return self._allParticipants
+        allParticipants.index = range(1, len(allParticipants) + 1)
+
+        return allParticipants
 
     def getByCode(self, code):
-        allParticipants = self.getAllParticipants()
+        allParticipants = self.allParticipants
         target = allParticipants[allParticipants["Code"] == code]
         return target
 
@@ -86,60 +89,89 @@ class Data():
         target = self.getByCode(code)
         return target.iat[0, 1]
 
-    def _getGroupLog(self, type, by):
-        allImportLog = pd.concat([
-            self.data[self.data["Trade Flow"] == "Import"],
-            self.data[self.data["Trade Flow"] == "Re-Import"]
-        ])
+    def getCountryLog(self, code, flow=None, who_report=None):
+        logs = self.data
 
-        allExportLog = pd.concat([
-            self.data[self.data["Trade Flow"] == "Export"],
-            self.data[self.data["Trade Flow"] == "Re-Export"]
-        ])
+        if who_report is None:
+            return pd.concat([
+                self.getCountryLog(code, flow, "self"),
+                self.getCountryLog(code, flow, "others"),
+            ]).reset_index(drop=True)
 
-        if(type == "Import"):
-            return allImportLog.groupby(by)
+        elif who_report == "self":
+            if flow is None:
+                return logs[logs["Reporter Code"] == code].reset_index(drop=True)
 
-        if(type == "Export"):
-            return allExportLog.groupby(by)
+            return logs[(logs["Reporter Code"] == code) & (logs["Trade Flow"] == flow)].reset_index(drop=True)
 
-    def getCountryLog(self, code, type, repoter="self"):
-        if (repoter == "self"):
-            return (self._getGroupLog(type, "Reporter Code")).get_group(code)
+        elif who_report == "others":
+            if flow is None:
+                return logs[logs["Partner Code"] == code].reset_index(drop=True)
+
+            elif flow == "Import":
+                return logs[(logs["Partner Code"] == code) & (logs["Trade Flow"] == "Export")].reset_index(drop=True)
+
+            elif flow == "Export":
+                return logs[(logs["Partner Code"] == code) & (logs["Trade Flow"] == "Import")].reset_index(drop=True)
+
+            else:
+                raise ValueError("flow must be Import, Export or None")
+
         else:
-            return (self._getGroupLog(type, "Partner Code")).get_group(code)
+            raise ValueError("who_report must be self, others or None")
 
     def getMergedData(self):
         if (self._mergedData is not None):
             return self._mergedData
-        allParticipants = self.getAllParticipants()
+        allParticipants = self.allParticipants
         tradeRecords = {}
 
         def getCooperationLog(u, v):
             for (partners, tradeLog) in tradeRecords.items():
                 if u == partners[0] and v == partners[1]:
                     return tradeLog
-            tradeRecords[(u, v)] = {"Import": 0, "Export": 0}
+            tradeRecords[(u, v)] = {
+                "Trade Value": {
+                    "reportFromU": 0,
+                    "reportFromV": 0
+                },
+                "Trade Quantity": {
+                    "reportFromU": 0,
+                    "reportFromV": 0
+                }
+            }
             return tradeRecords[(u, v)]
 
         for row in allParticipants.itertuples():
             try:
-                selfImportLog = self.getCountryLog(row.Code, "Import", "self")
+                selfImportLog = self.getCountryLog(row.Code, "Export", "self")
                 for log in selfImportLog.itertuples():
-                    getCooperationLog(log._3, log._1)["Import"] += log._6
+                    _log = getCooperationLog(log._1, log._3)
+                    _log["Trade Value"]["reportFromU"] += log._6
+                    _log["Trade Quantity"]["reportFromU"] += log._7
             except KeyError:
                 None
 
             try:
-                exportPartnerLog = self.getCountryLog(
-                    row.Code, "Export", "partner")
-                for log in exportPartnerLog.itertuples():
-                    getCooperationLog(log._1, log._3)["Export"] += log._6
+                selfExportLog = self.getCountryLog(row.Code, "Import", "self")
+                for log in selfExportLog.itertuples():
+                    _log = getCooperationLog(log._3, log._1)
+                    _log["Trade Value"]["reportFromV"] += log._6
+                    _log["Trade Quantity"]["reportFromV"] += log._7
             except KeyError:
                 None
 
+        def get_mean(arr):
+            if arr[0] != 0 and arr[1] != 0:
+                return int(sum(arr) / 2)
+            else:
+                return max(arr)
+
         for key, value in tradeRecords.items():
-            tradeRecords[key] = int((value["Import"] + value["Export"]) / 2)
+            tradeRecords[key]["Trade Value"] =\
+                get_mean(list(value["Trade Value"].values()))
+            tradeRecords[key]["Trade Quantity"] =\
+                get_mean(list(value["Trade Quantity"].values()))
 
         self._mergedData = tradeRecords
 
